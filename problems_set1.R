38259df77c38e94c43a282ea5b750fbc707d517d
Armando E <a_enriquez@live.com.mx>
2017-09-15 20:17:12 -0500

    adding dummy variables and interactions

--- problems_set1.R
+++ problems_set1.R
@@ -8,23 +8,41 @@
 #####################################
 
 
-## Data
-install.packages("AER")
+### ------------------------------------------------------
+### Part 1. Data
+### ------------------------------------------------------
 
+install.packages("AER")
 library(AER)
 data("GrowthDJ")
 
-names(GrowthDJ)
+### ------------------------------------------------------
+## Part 2.0 Rename variables names
+### ------------------------------------------------------
 
-library(dplyr)
+## Some variables names are uninformative.
+## Let's change some of them.
+
+names(GrowthDJ)
+library(dplyr) ## Tools for data manipulation
 
 ## Changing variables names
-GrowthDJ <- GrowthDJ %>% rename(oil_producer = oil, data_factor = inter,
+growth_data <- GrowthDJ %>% rename(oil_producer = oil, data_factor = inter,
                                 oecd_member = oecd, gdpgrowth_av = gdpgrowth, 
                                 popgrowth_av = popgrowth, invest_av = invest, 
                                 school_av = school)
 
+### -------------------------------------------------------
+## Part 2.1 Create variables
+### -------------------------------------------------------
+
 ## Dummies
 install.packages("dummies")
 library(dummies)
-GrowthDJ$oil_producer <- dummy(GrowthDJ$oil_producer)
+
+growth_data[, 1] <- dummy(growth_data[, 1]) ## For oil_producer
+growth_data[, 2] <- dummy(growth_data[, 2]) ## For data_factor
+growth_data[, 3] <- dummy(growth_data[, 3]) ## For oecd_member
+
+## Interaction
+growth_data$interact_gdp60_oilfactor <- growth_data$gdp60*growth_data$oil_producer
